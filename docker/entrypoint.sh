9d40380edf46b9df693e241f8b45d0b893c3fb66
luofeixue <luofeixue@co-mall.com>
2025-05-13 09:48:09 +0800

    适配MacOS环境运行

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -159,7 +159,7 @@ function start_mcp_server() {
 
 if [[ "${ENABLE_WEBSERVER}" -eq 1 ]]; then
     echo "Starting nginx..."
-    /usr/sbin/nginx
+    # /usr/sbin/nginx
 
     echo "Starting ragflow_server..."
     while true; do
